51969d66a6aabcdc36805a25cb3cf0470e529518
IAmKou <108041117+IAmKou@users.noreply.github.com>
2024-01-05 10:23:31 +0700

    Update house_finder.sql

--- house_finder.sql
+++ house_finder.sql
@@ -12,10 +12,11 @@ primary key (userID),
 fullName varchar(250),
 userName varchar(250),
 passWord varchar(250),
-role_id int,
+roleID int,
 location varchar(250),
 phone varchar(11),
-email varchar(250)
+email varchar(250),
+foreign key (roleID) references Role(id) 
 );
 CREATE TABLE TypeOfHouse (
     TypeOfHouseID INT PRIMARY KEY,
@@ -32,7 +33,7 @@ CREATE TABLE House (
     Picture VARCHAR(255),
     Available BOOLEAN,
     FOREIGN KEY (TypeOfHouseID) REFERENCES TypeOfHouse(TypeOfHouseID),
-    FOREIGN KEY (HouseOwnerID) REFERENCES User(userID)
+    FOREIGN KEY (HouseOwnerID) REFERENCES USER(userID)
 );
 CREATE TABLE SellPost (
     postID INT not null auto_increment,
@@ -42,7 +43,7 @@ CREATE TABLE SellPost (
     Time DATETIME,
     TypeOfHouseID INT,
     FOREIGN KEY (houseID) REFERENCES House(HouseID),
-    FOREIGN KEY (userID) REFERENCES User(userID),
+    FOREIGN KEY (userID) REFERENCES USER(userID),
     FOREIGN KEY (TypeOfHouseID) REFERENCES TypeOfHouse(TypeOfHouseID)
 );
 CREATE TABLE RequestSellPost (
@@ -55,7 +56,7 @@ CREATE TABLE OderBooking (
     userID INT,
     houseID INT,
     Time DATETIME,
-    FOREIGN KEY (userID) REFERENCES User(userID),
+    FOREIGN KEY (userID) REFERENCES USER(userID),
     FOREIGN KEY (houseID) REFERENCES House(HouseID)
 );
 CREATE TABLE RentingHouse (
@@ -68,13 +69,13 @@ CREATE TABLE RentingHouse (
     fee DECIMAL(10, 2),
     priceTotal DECIMAL(10, 2),
     FOREIGN KEY (HouseID) REFERENCES House(HouseID),
-    FOREIGN KEY (renterID) REFERENCES User(userID)
+    FOREIGN KEY (renterID) REFERENCES USER(userID)
 );
 CREATE TABLE Record (
     renterID INT,
     rhID INT,
     Time DATETIME,
-    FOREIGN KEY (renterID) REFERENCES User(userID),
+    FOREIGN KEY (renterID) REFERENCES USER(userID),
     FOREIGN KEY (rhID) REFERENCES RentingHouse(rhID)
 );
 CREATE TABLE Feedback (
@@ -85,7 +86,7 @@ CREATE TABLE Feedback (
     grade INT,
     FeedBack VARCHAR(255),
     Time DATETIME,
-    FOREIGN KEY (userID) REFERENCES User(userID),
+    FOREIGN KEY (userID) REFERENCES USER(userID),
     FOREIGN KEY (rhID) REFERENCES RentingHouse(rhID)
 );
 
