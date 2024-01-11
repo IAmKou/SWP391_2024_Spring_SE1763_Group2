89bf78e19af20d89308a791bc7bb1794ae1b01a4
IAmKou <luongcu111@gmail.com>
2024-01-11 14:45:32 +0700

    update registerController

--- src/java/Servlet/registerController.java
+++ src/java/Servlet/registerController.java
@@ -12,6 +12,7 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import model.user;
 
 /**
@@ -53,61 +54,54 @@ public class registerController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        try {
+        
+          try {
+            HttpSession session = request.getSession();
             userDAO dao = new userDAO();
             String msg = "";
-            boolean flag = true;
             //Get data from page
-            String userName = request.getParameter("username");
-            String passWord = request.getParameter("password");
-            String cfPassWord = request.getParameter("cfpassword");
-            String fullName = request.getParameter("fullname");
-            
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
+            String repeatPassword = request.getParameter("cfpassword");
+            String fullname = request.getParameter("fullname");
             String email = request.getParameter("email");
             String phone = request.getParameter("phone");
             String location = request.getParameter("location");
-            int roleid = Integer.parseInt(request.getParameter("roleid"));
+            int roleid = 1;
             
-             if (dao.userNameIsExist(userName)) {
+            //Validate phone 
+            if (dao.phoneIsExist(phone)) {
                 msg = "This username has already existed!!!";
-                flag = false;
+                request.getRequestDispatcher("signUp.jsp").forward(request, response);
             }
-            // validate password
-            if (!passWord.equals(cfPassWord)) {
-                msg = "Repeat password is not correct";
-                flag = false;
-            }
-            if (dao.emailIsExist(email)) {
+//            // validate password
+            else if (!password.equals(repeatPassword)) {
+                msg="Confirm password not match password. Please enter again!";
+              request.getRequestDispatcher("signUp.jsp").forward(request, response);
+            }//validate email
+            else if (dao.emailIsExist(email)) {
                 msg = "This email has already existed!!!";
-                flag = false;
+                request.getRequestDispatcher("signUp.jsp").forward(request, response);
             }
-            //Add user to DB
-            if (flag = true) {
-                //If flag = true, add User to db
+//            //Add user to DB
+            else{
                 user u = new user();
-                u.setUserName(userName);
-                u.setPassWord(passWord);
-                u.setFullName(fullName);
+                u.setUserName(username);
+                u.setPassWord(password);
+                u.setFullName(fullname);
                 u.setEmail(email);
                 u.setPhone(phone);
                 u.setLocation(location);
                 u.setRoleID(roleid);
                 dao.insertUser(u);
-                //msg = "Add user successfully!!!"; 
-               request.getRequestDispatcher("mainPage.jsp").forward(request, response);
-            }
-            else {
-                //If flag = false, send msg to page
-                request.setAttribute("msg", msg);
-                request.getRequestDispatcher("signUp.jsp").forward(request, response);
-            }
-        }     catch (Exception e) {
+                request.getRequestDispatcher("mainPage.jsp").forward(request, response);
+            }                   
+        } 
+        catch (Exception e) {
             System.out.println(e.getMessage());
-                           }
-    }
-        
+        }
 
-    
+    }
 
     /** 
      * Returns a short description of the servlet.
