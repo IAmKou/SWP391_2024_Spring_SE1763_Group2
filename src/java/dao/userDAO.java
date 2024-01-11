64943ec8d2534e081d542e18b28a89dc1269ec58
IAmKou <luongcu111@gmail.com>
2024-01-11 14:45:44 +0700

    update userDAO

--- src/java/dao/userDAO.java
+++ src/java/dao/userDAO.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import model.user;
 
 /**
@@ -22,14 +23,13 @@ public class userDAO {
             Connection con = db.getConnection();
 
             // Prepare the SQL statement
-            String sql = "INSERT INTO USER (userName, passWord, fullName,"
-                    + "roleID,location , phone, email)"
+            String sql = "INSERT INTO USER (fullName, userName, passWord, roleID, location, phone, email)"
                     + "VALUES (?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement st = con.prepareStatement(sql);
             st = con.prepareStatement(sql);
-            st.setString(1, user.getUserName());
-            st.setString(2, user.getPassWord());
-            st.setString(3, user.getFullName());
+            st.setString(1, user.getFullName());
+            st.setString(2, user.getUserName());
+            st.setString(3, user.getPassWord());
             st.setInt(4, user.getRoleID());
             st.setString(5, user.getLocation());
             st.setString(6, user.getPhone());
@@ -46,13 +46,13 @@ public class userDAO {
             System.out.println(e.getMessage());
         }
     }
-        public boolean userNameIsExist(String username) {
+        public boolean phoneIsExist(String phone) {
         try {
             DBContext db = new DBContext();
             Connection con = db.getConnection();
-            String query = "select count(*) as num from [USER] where userName = ?";
+            String query = "select count(*) as num from USER where phone = ?";
             PreparedStatement ps = con.prepareStatement(query);
-            ps.setString(1, username);
+            ps.setString(1, phone);
             ResultSet rslt = ps.executeQuery();
             if (rslt.next()) {
                 return Integer.parseInt(rslt.getString(1)) > 0;
@@ -66,7 +66,7 @@ public class userDAO {
         try {
             DBContext db = new DBContext();
             Connection con = db.getConnection();
-            String query = "select count(*) as num from [USER] where email = ?";
+            String query = "select count(*) as num from USER where email = ?";
             PreparedStatement ps = con.prepareStatement(query);
             ps.setString(1, email);
             ResultSet rslt = ps.executeQuery();
@@ -76,5 +76,29 @@ public class userDAO {
         } catch (SQLException e) {
         }
         return false;
+    }
+     public static user LogIn(String username, String pass) {
+        user user = null;
+        try {
+            DBContext db = new DBContext();
+            Connection con = db.getConnection();
+            if (con != null) {
+                String sql = "Select * from USER where userName = " + "'" + username + "'"
+                        + "AND passWord = " + "'" + pass + "' limit 1";
+                Statement call = con.createStatement();
+                ResultSet rs = call.executeQuery(sql);
+                while (rs.next()) {
+                    user = new user(rs.getInt("userId"),
+                            username, pass, rs.getString("fullName"), rs.getInt("roleId"),
+                            rs.getString("location"), rs.getString("phone"),
+                            rs.getString("email"));
+                }
+                call.close();
+                con.close();
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return user;
     }
 }
