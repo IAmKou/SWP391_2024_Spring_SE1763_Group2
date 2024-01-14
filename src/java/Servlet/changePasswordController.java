fdd6b3cd7195223f13c952856ee28955c6533fe2
IAmKou <luongcu111@gmail.com>
2024-01-14 18:45:07 +0700

    update changePasswordController

--- src/java/Servlet/changePasswordController.java
+++ src/java/Servlet/changePasswordController.java
@@ -32,51 +32,39 @@ public class changePasswordController extends HttpServlet {
      */
 
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
-     * Handles the HTTP <code>GET</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        
-    } 
-
-    /** 
-     * Handles the HTTP <code>POST</code> method.
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String oldPass = request.getParameter("oldpass");
         String newPass = request.getParameter("newpass");
         String newCfPass = request.getParameter("cfpass");
         userDAO dao = new userDAO();
         HttpSession ses = request.getSession();
-        user user = (user) request.getSession().getAttribute("acc");
+        user user = (user) request.getSession().getAttribute("account");
         if (!oldPass.equals(user.getPassWord())) {
-            ses.setAttribute("mess1", "Old password not match");
-            request.getRequestDispatcher("ChangePassword.jsp").forward(request, response);
+            request.setAttribute("mess1", "Old password not match");
+            request.getRequestDispatcher("changePassword.jsp").forward(request, response);
         } else {
             if (newPass.equals(newCfPass)) {
                 dao.ChangePassword(user.getUserID(), newPass);
-                ses.invalidate();
-                request.getRequestDispatcher("login.jsp").forward(request, response);
+                request.getRequestDispatcher("mainPage.jsp").forward(request, response);
             } else {
-                ses.setAttribute("mess1", "New pass and confirm not match");
-                request.getRequestDispatcher("ChangePassword.jsp").forward(request, response);
+                request.setAttribute("mess1", "New pass and confirm not match");
+                request.getRequestDispatcher("changePassword.jsp").forward(request, response);
             }
         }
     }
 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
     /** 
      * Returns a short description of the servlet.
      * @return a String containing servlet description
