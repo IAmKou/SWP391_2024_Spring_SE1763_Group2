13a111e170d3afc9e9cdf4d47111a495192449f0
hoanganh30082003 <hn54707@gmail.com>
2024-01-16 10:45:33 +0700

    create order

---
+++ src/java/dao/OrderDAO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import Context.DBContext;
+import model.Order;
+
+/**
+ *
+ * @author FPTSHOP
+ */
+public class OrderDAO extends DBContext{
+    public Order getOder(){
+        
+        
+        
+        return null;
+    }
+}
---
+++ src/java/model/Order.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Time;
+
+/**
+ *
+ * @author FPTSHOP
+ */
+public class Order {
+    User user;
+    House house;
+    Time time;
+
+    public Order() {
+    }
+
+    public Order(User user, House house, Time time) {
+        this.user = user;
+        this.house = house;
+        this.time = time;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public House getHouse() {
+        return house;
+    }
+
+    public void setHouse(House house) {
+        this.house = house;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+    
+}
