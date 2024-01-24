4e7f40eba16c73cc70d3d433557f5fb4931d8266
IAmKou <luongcu111@gmail.com>
2024-01-25 02:40:57 +0700

    Update resetPassController.java

--- src/java/Controller/resetPassController.java
+++ src/java/Controller/resetPassController.java
@@ -14,6 +14,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import model.account;
 import model.user;
 
 /**
@@ -36,9 +37,9 @@ public class resetPassController extends HttpServlet {
         String newCfPass = request.getParameter("cfpass");
         userDAO dao = new userDAO();
         HttpSession session = request.getSession();
-        user newUser = (user) request.getSession().getAttribute("userForgetPass");
+        account newUser = (account) request.getSession().getAttribute("userForgetPass");
         if (newPass.equals(newCfPass)) {
-            dao.ChangePassword(newUser.getUserID(), newPass);
+            dao.ChangePassword(newUser.getUser_id(), newPass);
             session.invalidate();
             request.getRequestDispatcher("logIn.jsp").forward(request, response);
         } else {
