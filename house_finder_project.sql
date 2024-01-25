643d1680e58f05f0be2b6a06a71d738e75ad4021
IAmKou <108041117+IAmKou@users.noreply.github.com>
2024-01-25 22:17:20 +0700

    Update house_finder_project.sql

--- house_finder_project.sql
+++ house_finder_project.sql
@@ -316,7 +316,7 @@ UNLOCK TABLES;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL auto_increment,
   `full_name` varchar(250) DEFAULT NULL,
   `date_of_birth` date DEFAULT NULL,
   `address` varchar(250) DEFAULT NULL,
