2aedf049fb6ec1e93403662d8ef4060edb28a6fe
IAmKou <luongcu111@gmail.com>
2024-03-02 16:40:28 +0700

    Update changePasswordController.java

--- src/java/Controller/changePasswordController.java
+++ src/java/Controller/changePasswordController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package Controller;
 
 import dao.UserDAO;
@@ -19,19 +18,19 @@ import model.User;
  *
  * @author ACER
  */
-@WebServlet(name="changePasswordController", urlPatterns={"/changePasswordController"})
+@WebServlet(name = "changePasswordController", urlPatterns = {"/changePasswordController"})
 public class changePasswordController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-
-
-     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String oldPass = request.getParameter("oldpass");
         String newPass = request.getParameter("newpass");
@@ -39,14 +38,17 @@ public class changePasswordController extends HttpServlet {
         UserDAO dao = new UserDAO();
         User user = (User) request.getSession().getAttribute("account");
         int uid = user.getUser_id();
-        Account account = dao.getAccount(uid);        
-        if (!oldPass.equals(account.getPass_word())){
+        Account account = dao.getAccount(uid);
+        if (!oldPass.equals(account.getPass_word())) {
             request.setAttribute("mess1", "Old password not match");
             request.getRequestDispatcher("changePassword.jsp").forward(request, response);
         } else {
             if (newPass.equals(newCfPass)) {
                 dao.ChangePassword(user.getUser_id(), newPass);
-                request.getRequestDispatcher("profile.jsp").forward(request, response);
+                String msg = "Change password successfully";
+                String redirectURL = request.getContextPath() + "/viewProfile";
+                redirectURL += "?message=" + msg;
+                response.sendRedirect(redirectURL);
             } else {
                 request.setAttribute("mess1", "New pass and confirm not match");
                 request.getRequestDispatcher("changePassword.jsp").forward(request, response);
@@ -65,8 +67,10 @@ public class changePasswordController extends HttpServlet {
             throws ServletException, IOException {
         processRequest(request, response);
     }
-    /** 
+
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
