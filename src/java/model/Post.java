cbd2e7c5c2dabb050d66916688cdf3a04507cc0a
IAmKou <luongcu111@gmail.com>
2024-03-10 22:43:35 +0700

    Update Post.java

--- src/java/model/Post.java
+++ src/java/model/Post.java
@@ -19,17 +19,19 @@ public class Post {
     private int poster_id;
     private Status house_status;
     private int admin_id;
+    private int active_feedback;
+    private float feedback_point;
     private Status post_status;
     private LocalDateTime start_time;
     private LocalDateTime end_time;
-    private LocalDateTime create_time;
-    private String fommated_create_time;
+//    LocalDateTime create_time;
+//    private String fommated_create_time;
     private String admin_message;
 
     public Post() {
     }
 
-    public Post(int post_id, House house, Purpose purpose, int price, int poster_id, Status house_status, int admin_id, Status post_status, LocalDateTime start_time, LocalDateTime end_time) {
+    public Post(int post_id, House house, Purpose purpose, int price, int poster_id, Status house_status, int admin_id, int active_feedback, float feedback_point, Status post_status, LocalDateTime start_time, LocalDateTime end_time, String admin_message) {
         this.post_id = post_id;
         this.house = house;
         this.purpose = purpose;
@@ -37,113 +39,117 @@ public class Post {
         this.poster_id = poster_id;
         this.house_status = house_status;
         this.admin_id = admin_id;
+        this.active_feedback = active_feedback;
+        this.feedback_point = feedback_point;
         this.post_status = post_status;
         this.start_time = start_time;
         this.end_time = end_time;
+        this.admin_message = admin_message;
     }
 
-    public String getFommated_create_time() {
-        return fommated_create_time;
-    }
-
-    public void setFommated_create_time(String fommated_create_time) {
-        this.fommated_create_time = fommated_create_time;
+    public void setAdmin_message(String admin_message) {
+        this.admin_message = admin_message;
     }
 
     public String getAdmin_message() {
         return admin_message;
     }
 
-    public void setAdmin_message(String admin_message) {
-        this.admin_message = admin_message;
-    }
-    
     public void setStart_time(LocalDateTime start_time) {
         this.start_time = start_time;
-    }
-
-    public void setEnd_time(LocalDateTime end_time) {
-        this.end_time = end_time;
-    }
-
-    public LocalDateTime getStart_time() {
-        return start_time;
-    }
-
-    public LocalDateTime getEnd_time() {
-        return end_time;
-    }
-
-    public int getPost_id() {
-        return post_id;
-    }
-
-    public void setPost_id(int post_id) {
-        this.post_id = post_id;
-    }
-
-    public House getHouse() {
-        return house;
-    }
-
-    public void setHouse(House house) {
-        this.house = house;
-    }
-
-    public Purpose getPurpose() {
-        return purpose;
     }
 
     public void setPurpose(Purpose purpose) {
         this.purpose = purpose;
     }
 
-    public int getPrice() {
-        return price;
-    }
-
     public void setPrice(int price) {
         this.price = price;
-    }
-
-    public int getPoster_id() {
-        return poster_id;
     }
 
     public void setPoster_id(int poster_id) {
         this.poster_id = poster_id;
     }
 
-    public Status getHouse_status() {
-        return house_status;
+    public void setPost_status(Status post_status) {
+        this.post_status = post_status;
+    }
+
+    public void setPost_id(int post_id) {
+        this.post_id = post_id;
     }
 
     public void setHouse_status(Status house_status) {
         this.house_status = house_status;
     }
 
-    public int getAdmin_id() {
-        return admin_id;
+    public void setHouse(House house) {
+        this.house = house;
+    }
+
+    public void setFeedback_point(float feedback_point) {
+        this.feedback_point = feedback_point;
+    }
+
+    public void setEnd_time(LocalDateTime end_time) {
+        this.end_time = end_time;
     }
 
     public void setAdmin_id(int admin_id) {
         this.admin_id = admin_id;
     }
 
+    public void setActive_feedback(int active_feedback) {
+        this.active_feedback = active_feedback;
+    }
+
+    public LocalDateTime getStart_time() {
+        return start_time;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public Purpose getPurpose() {
+        return purpose;
+    }
+
+    public int getPoster_id() {
+        return poster_id;
+    }
+
+    public int getPost_id() {
+        return post_id;
+    }
+
     public Status getPost_status() {
         return post_status;
     }
 
-    public void setPost_status(Status post_status) {
-        this.post_status = post_status;
+    public Status getHouse_status() {
+        return house_status;
     }
 
-    public LocalDateTime getCreate_time() {
-        return create_time;
+    public House getHouse() {
+        return house;
     }
 
-    public void setCreate_time(LocalDateTime create_time) {
-        this.create_time = create_time;
+    public float getFeedback_point() {
+        return feedback_point;
     }
+
+    public LocalDateTime getEnd_time() {
+        return end_time;
+    }
+
+    public int getAdmin_id() {
+        return admin_id;
+    }
+
+    public int getActive_feedback() {
+        return active_feedback;
+    }
+
 
 }
