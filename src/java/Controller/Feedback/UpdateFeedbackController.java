7ec2684394ed46482441c48cb3b161f84d7a2ea8
IAmKou <luongcu111@gmail.com>
2024-03-12 08:03:24 +0700

    Update UpdateFeedbackController.java

--- src/java/Controller/Feedback/UpdateFeedbackController.java
+++ src/java/Controller/Feedback/UpdateFeedbackController.java
@@ -5,6 +5,7 @@
 
 package Controller.Feedback;
 
+import dao.FeedbackDAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -12,6 +13,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import model.feedback;
 
 /**
  *
@@ -29,19 +33,19 @@ public class UpdateFeedbackController extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet UpdateFeedbackController</title>");  
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet UpdateFeedbackController at " + request.getContextPath () + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        String content = request.getParameter("content");
+        int uid = Integer.parseInt(request.getParameter("uid"));
+        int pid = Integer.parseInt(request.getParameter("pid"));
+        int fid = Integer.parseInt(request.getParameter("fid"));
+        
+        FeedbackDAO dao = new FeedbackDAO();
+        dao.updateFeedback(fid,uid,content,pid);
+        ArrayList<feedback> f = dao.getAllFeedbackInAPost(pid);
+        HttpSession ses = request.getSession();
+        ses.removeAttribute("feedback");
+        ses.setAttribute("feedback", f);
+        request.setAttribute("msg", "Update sucess");
+        request.getRequestDispatcher("/views/post.jsp").forward(request, response);
     } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
